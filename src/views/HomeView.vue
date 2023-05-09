31c0b8ad21c0b4485bf1393e90c04ca8df43c928
LykeSungahid <17100098@usc.edu.ph>
2023-05-09 12:48:05 +0800

    Added Files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to the Official Klima Engineering Services Website</h1>
+  </main>
+</template>
